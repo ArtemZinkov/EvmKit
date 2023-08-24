7770a4614b5b33fcf63c858b4aff78c0e5204c78
Artem Zinkov <spdzinkov@gmail.com>
2023-08-24 14:22:50 +0200

    added bip32

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
           .package(url: "https://github.com/groue/GRDB.swift.git", .upToNextMajor(from: "5.0.0")),
           .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0")),
           .package(url: "https://github.com/horizontalsystems/HsCryptoKit.Swift.git", .upToNextMajor(from: "1.2.1")),
-          .package(url: "https://github.com/horizontalsystems/HdWalletKit.Swift.git", .upToNextMajor(from: "1.2.1")),
+          .package(url: "https://github.com/ArtemZinkov/HdWalletKit.git", .branch("main")),
           .package(url: "https://github.com/horizontalsystems/HsToolKit.Swift.git", .upToNextMajor(from: "2.0.0")),
           .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
         ],
@@ -31,7 +31,7 @@ let package = Package(
                     .product(name: "GRDB", package: "GRDB.swift"),
                     "ObjectMapper",
                     .product(name: "HsCryptoKit", package: "HsCryptoKit.Swift"),
-                    .product(name: "HdWalletKit", package: "HdWalletKit.Swift"),
+                    .product(name: "HdWalletKit", package: "HdWalletKit"),
                     .product(name: "HsToolKit", package: "HsToolKit.Swift"),
                     .product(name: "HsExtensions", package: "HsExtensions.Swift"),
                   ]
